d2b2f39fa04a633abfaf2777dc6fb9b5c29e610e
novaspot <novaspot.corp@gmail.com>
2025-12-27 19:52:10 +0530

    added 2 edittext

--- app/src/main/java/com/example/scratch1/feature/auth/signin/SignInScreen.kt
+++ app/src/main/java/com/example/scratch1/feature/auth/signin/SignInScreen.kt
@@ -1,19 +1,32 @@
 package com.example.scratch1.feature.auth.signin
 
+import android.R.attr.label
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
 fun SignInScreen(navController: NavController){
     Scaffold(modifier = Modifier.fillMaxSize()) {
-        Column(modifier = Modifier.padding(it)) {
+        Column(modifier = Modifier.padding(it).fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Text("hello")
+            OutlinedTextField(value = "", onValueChange = {})
+            Spacer(modifier = Modifier.padding(10.0.dp))
+            OutlinedTextField(value = "",onValueChange = {})
         }
     }
 }
